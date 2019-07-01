9fbd5c4f07ff9b564b6c0762990c3e136e4fb34e
thongcam <45142231+thongcam@users.noreply.github.com>
2019-07-01 22:25:51 +0700

    Updates

---
+++ precache-manifest.2693549f72fbc085f183ab30bc0b7bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e8ad6d568a7c2fec27078ab375b6bc",
+    "url": "/star-war-search-engine/./index.html"
+  },
+  {
+    "revision": "abfe9d8529c7a87f92bb",
+    "url": "/star-war-search-engine/./static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a6488212749bfeaf1cad",
+    "url": "/star-war-search-engine/./static/css/main.05df0a9e.chunk.css"
+  },
+  {
+    "revision": "abfe9d8529c7a87f92bb",
+    "url": "/star-war-search-engine/./static/js/2.15703758.chunk.js"
+  },
+  {
+    "revision": "a6488212749bfeaf1cad",
+    "url": "/star-war-search-engine/./static/js/main.a9321666.chunk.js"
+  },
+  {
+    "revision": "3d43760b2a0e8f187794",
+    "url": "/star-war-search-engine/./static/js/runtime~main.c535d77d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-war-search-engine/./static/media/logo.5d5d9eef.svg"
+  }
+]);
