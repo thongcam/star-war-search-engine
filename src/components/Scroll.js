43ae161d44159d478771ff70226ab8f6150443c1
thongcam <45142231+thongcam@users.noreply.github.com>
2019-07-05 11:28:02 +0700

    Momentum Scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return(
-    <div style={{overflowY:'scroll',height:'65vh',width:'95vw'}}>
+    <div style={{overflowY:'scroll',height:'65vh',width:'95vw',-webkit-overflow-scrolling:'touch'}}>
       {props.children}
     </div>
   )
